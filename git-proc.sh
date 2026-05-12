51ab2509ef2b16f319bae25223b3c2481a0467ae
Song <song@example.com>
2026-05-12 13:56:19 -0500

    2026-05-12 13:56
    Updated

--- git-proc.sh
+++ git-proc.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+set -e
 
 ADDITIONAL_COMMENT="$1"
-git add .
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M')
-if [ -n "$ADDITIONAL_COMMENT" ]; then
-    git commit -m "$TIMESTAMP: $ADDITIONAL_COMMENT"
-else
-    git commit -m "$TIMESTAMP" -m "Updated"
-fi
-git push
 
+git add .
+
+if ! git diff --cached --quiet; then
+    TIMESTAMP=$(date '+%Y-%m-%d %H:%M')
+    if [ -n "$ADDITIONAL_COMMENT" ]; then
+        git commit -m "$TIMESTAMP: $ADDITIONAL_COMMENT"
+    else
+        git commit -m "$TIMESTAMP" -m "Updated"
+    fi
+else
+    echo "Nothing to commit."
+fi
+
+git pull --rebase origin main
+git push
