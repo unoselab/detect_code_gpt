6dd0752b3003d7a09581ba940e112c4485ca8df3
msong <44676836@nebraska.edu>
2026-05-18 01:02:52 -0500

    2026-05-18 01:02
    Updated

--- run7-plot-threshold-sweep.sh
+++ run7-plot-threshold-sweep.sh
@@ -9,5 +9,5 @@ python code-detection/plot_threshold_sweep.py \
   --csv logs/benchmark_results_benchmark_level1_starcoder2-7b.csv \
   --n_samples 530 \
   --threshold_min 1.0 \
-  --threshold_max 2.2 \
+  --threshold_max 2.0 \
   --output_image logs/threshold_sweep_starcoder2-7b.png
